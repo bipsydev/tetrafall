b684ee7233d0185e383dcad29f279523c0076b41
Lily-Heather Crawford <lilithalicecrawford@gmail.com>
2025-05-13 13:35:55 -0500

    Refactor `GameScreen` a bit
    Made update and updateFrameData functions, reorganized some code a bit, made some game variables class members, and revised some comments.

--- core/src/main/kotlin/zone/slime/tetrafall/GameScreen.kt
+++ core/src/main/kotlin/zone/slime/tetrafall/GameScreen.kt
@@ -1,6 +1,7 @@
 package zone.slime.tetrafall
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
@@ -17,7 +18,7 @@ import zone.slime.tetrafall.TetraFall as TF
 
 /**
  *******************************************************************************
- * GameScreen class
+ * `GameScreen` class
  *******************************************************************************
  * This `KtxScreen` subclass represents the main game screen view.
  * It controls all game-related variables contained within the view,
@@ -26,6 +27,7 @@ import zone.slime.tetrafall.TetraFall as TF
  */
 class GameScreen : KtxScreen {
 
+    // --- companion object ---
     // store texture references here, load them with the global asset manager
     companion object {
 
@@ -36,29 +38,40 @@ class GameScreen : KtxScreen {
         private fun txStr(i: Int) = "PhotonicBox/fotonicbox$i.png"
 
         /**
-         * `TX_FOTONICBOX` - Array of Asset wrappers holding Texture resource refs
-         *-------------------------------------------------------------------------
+         * `TX_FOTONICBOX` - Array of Texture references
+         *-----------------------------------------------
          * Store all images in this array of `Asset<Texture>`
-         * `Asset` is a wrapper around the Texture resource,
-         * useful for `Asset.finishLoading()` and other loading methods.
+         * Assets are loaded with `AssetStorage` instance `TetraFall.ASSETS`
+         * and references to the resources are stored in this array.
+         * This way, the screen can access these resources globally/statically.
          */
         lateinit var TX_FOTONICBOX: Array<Texture>
 
-        /** This flag will be set to `true` when all `TX_FOTONICBOX` elements are loaded.*/
+        /**
+         * This flag will be set to `true` when
+         * all `TX_FOTONICBOX` elements are loaded.
+         */
         var TX_FOTONICBOX_LOADED: Boolean = false
 
         // Images Source: https://el-chucho.itch.io/fotonicboxes
     }
-    // end of companion object
+    // --- end of companion object ---
 
-    // scene active running time
+    /** scene active running time */
     private var time: Double = 0.0
 
-    // scene active frame `render` count
+    /** scene active frame `render` count */
     private var frame: Int = 0
 
-    // the current FPS calculated from given `delta`.
+    /** the current FPS calculated from given `delta`. */
     private var fps: Double = 0.0
+
+    /** The viewport's minimum/desired dimensions. */
+    private var dims: Vector2 = Vector2(0f, 0f)
+    /** The minimum of either `dims.x` or `dims.y`. */
+    private var dimMin: Float = 0f
+    /** Position to draw the floating square + frame number text. */
+    private var rectPos: Vector2 = Vector2(0f, 0f)
 
     /**
      * GameScreen initialization block
@@ -85,6 +98,7 @@ class GameScreen : KtxScreen {
             LOG.debug { "TX_FOTONICBOX loading coroutine over." }
             TX_FOTONICBOX_LOADED = true
         }
+
         LOG.debug { "GameScreen successfully initialized." }
     }
 
@@ -99,113 +113,173 @@ class GameScreen : KtxScreen {
     }
 
     /**
-     * `render` method override
-     *--------------------------
-     * Called every time the game needs a frame update.
-     * This is where the game should react to events, update game logic,
-     * and then render to the screen.
+     * `updateFrameData` private method
+     *----------------------------------
+     * Updates FPS, time, frame count, and other frame-time data.
+     *
+     * @param delta - the time in seconds since last frame, passed by `render`.
      */
-    override fun render(delta: Float) {
-        fps = 1.0 / delta.toDouble()    // update FPS calculation with new delta
-        if (fps == Double.NEGATIVE_INFINITY || fps == Double.POSITIVE_INFINITY) {
-            fps = 0.0
+    private fun updateFrameData(delta: Float) {
+        // no time or frame increase on first frame
+        if (frame > 0) {
+            time += delta   // update scene time
+            frame++         // increase frame count
         }
+        // update FPS calculation with new delta
+        //TODO add smoothing
+        val smoothingFactor: Double = 0.9
+        fps = (1.0 / delta.toDouble()).takeIf { it.isFinite() } ?: 0.0
+        // calculates 1/delta, sets to 0 if result is either infinity
+    }
+
+    /**
+     * `update` private method
+     *-------------------------
+     * Called in `render` after `updateFrameData` before any rendering is done.
+     * Updates game logic based on events & input.
+     *
+     * @param delta - the time in seconds since last frame, passed by `render`.
+     */
+    private fun update(delta: Float) {
 
         // log this sometimes
         if (frame < 25
             || (frame % 25 == 0 && frame <= 1000)
             || frame % 1000 == 0) {
             LOG.debug {
-                "frame # %3d: rendering GameScreen with delta = %.4f (%.2f FPS), time = %.2f"
-                    .format(frame, delta, fps, time)
+                ("frame # %3d: rendering GameScreen with delta = %.4f " +
+                    "(%.2f FPS), time = %.2f")
+                        .format(frame, delta, fps, time)
             }
         }
+
+        // if we have our textures loaded...
+        if (TX_FOTONICBOX_LOADED) {
+
+            // calculate some things into private properties (render data)
+            dims = Vector2(TF.VIEWPORT.minWorldWidth,
+                           TF.VIEWPORT.minWorldHeight)
+            dimMin = min(dims.x, dims.y)
+            rectPos = Vector2(frame/4.5f, frame/8f)
+
+            // pan camera based on delta mouse pos (movement)
+            if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+                TF.CAMERA.translate(
+                    -Gdx.input.deltaX.toFloat(),
+                    Gdx.input.deltaY.toFloat()
+                )
+            }
+
+        }
+
+    }
+
+    /**
+     * `render` method override
+     *--------------------------
+     * Called every time the game needs a frame update.
+     * This is where the game should react to events, update game logic,
+     * and then render to the screen.
+     *
+     * @param delta - A `Float`, the time in seconds since the last frame.
+     *                Passed in by libGDX for us to use for the frame.
+     */
+    override fun render(delta: Float) {
+        // update time, FPS & frame count
+        updateFrameData(delta)
+
+        // update game logic
+        update(delta)
 
         // show "loading..." text while loading textures
         if (!TX_FOTONICBOX_LOADED)
         {
-            // display text
-            TF.BATCH.use {
-                TF.FONT.data.setScale(4f)
-                TF.FONT.color = Color.WHITE
-                TF.FONT.draw(TF.BATCH, "LOADING...", 32.0f, 128.0f)
-                TF.FONT.data.setScale(2f)
-            }
+            drawLoading()
         }
         else
         {
-            val dims = Vector2(TF.VIEWPORT.minWorldWidth, TF.VIEWPORT.minWorldHeight)
-            val dimMin = min(dims.x, dims.y)
-            val rectPos = Vector2(frame/4.5f, frame/8f)
-
-            // draw filled
-            TF.SHAPES.use(ShapeRenderer.ShapeType.Filled) {
-                // draw a green rectangle on entire screen
-                TF.SHAPES.color = Color.GREEN
-                //SHAPES.rect(0f, 0f, vdims.x, vdims.y)
-                TF.SHAPES.rect(0f, 0f, dims.x, dims.y)
-                // draw circles in top-right corner
-                TF.SHAPES.color = Color.RED
-                /*
-                LOG.debug { "SHAPES.circle(${vdims.x}, ${vdims.y}, ${ppiMinScale / 4f})" }
-                TF.SHAPES.circle(vdims.x, vdims.y, vdimsMin / 4f)
-                */
-                TF.SHAPES.circle(dims.x, dims.y, dimMin / 4f)
-                TF.SHAPES.color = Color.MAGENTA
-                //SHAPES.circle(vdims.x, vdims.y, vdimsMin / 8f)
-                TF.SHAPES.circle(dims.x, dims.y, dimMin / 8f)
-
-                // Draw moving white box with red dot, moves with frame count
-                TF.SHAPES.color = Color.WHITE
-                TF.SHAPES.rect(rectPos.x - 16f, rectPos.y - 16f,
-                    32f, 32f)
-                TF.SHAPES.color = Color.RED
-                TF.SHAPES.circle(rectPos, 3f)
-            }
-            TF.BATCH.use {
-                TF.FONT.data.setScale(1.25f)
-                TF.FONT.color = Color.BLACK
-                TF.FONT.draw(TF.BATCH,
-                    "FRAME: $frame",
-                    rectPos.x, rectPos.y)
-
-                TF.FONT.data.setScale(2f)
-                TF.FONT.color = Color.RED
-                TF.FONT.draw(TF.BATCH,
-                    "Viewport Screen: ${TF.VIEWPORT.screenWidth} x ${TF.VIEWPORT.screenHeight}",
-                    32f, 132f)
-
-                TF.FONT.color = Color.BLUE
-                TF.FONT.draw(TF.BATCH,
-                    "Viewport World: ${TF.VIEWPORT.worldWidth} x ${TF.VIEWPORT.worldHeight}",
-                    32f, 164f)
-
-                TF.FONT.draw(TF.BATCH,
-                    "dims (min): ${dims.x}, ${dims.y}",
-                    32f, 196f)
-
-                TF.FONT.color = Color.WHITE
-                TF.FONT.draw(TF.BATCH,
-                    "Graphics Density: ${Gdx.graphics.density}",
-                    32f, 228f)
-
-                TF.FONT.draw(TF.BATCH,
-                    "PPI X & Y: ${Gdx.graphics.ppiX}, ${Gdx.graphics.ppiY}",
-                    32f, 260f)
-
-                /*
-                TF.FONT.draw(TF.BATCH,
-                    "PPI X & Y / target PPI: ${ppiXScale}, ${ppiYScale}",
-                    32f, 260f)
-                TF.FONT.draw(TF.BATCH,
-                    "vdims: ${TF.VIEWPORT.worldWidth * ppiMinScale}, ${TF.VIEWPORT.worldHeight * ppiMinScale}",
-                    32f, 292f)
-                 */
-            }
+            drawRenderingTest()
         }
+    }
 
-        time += delta   // update scene time
-        frame++         // increase frame count
+    /**
+     * `drawLoading` private method
+     *--------------------------------
+     * This is what is rendered while we wait for resources to load.
+     * Just some simple text for now.
+     */
+    private fun drawLoading() {
+        // display text
+        TF.BATCH.use {
+            TF.FONT.data.setScale(4f)   // make larger
+            TF.FONT.color = Color.WHITE
+            //TODO position in middle
+            TF.FONT.draw(TF.BATCH, "LOADING...", 32.0f, 128.0f)
+            TF.FONT.data.setScale(2f)   // make smaller (revert)
+        }
+    }
+
+    private fun drawRenderingTest() {
+        // ======================== DRAW FILLED SHAPES =========================
+        TF.SHAPES.use(ShapeRenderer.ShapeType.Filled) {
+            // draw a green rectangle on entire screen
+            TF.SHAPES.color = Color.GREEN
+            TF.SHAPES.rect(0f, 0f, dims.x, dims.y)
+            // draw circles in top-right corner
+            TF.SHAPES.color = Color.RED
+            TF.SHAPES.circle(dims.x, dims.y, dimMin / 4f)
+            TF.SHAPES.color = Color.MAGENTA
+            TF.SHAPES.circle(dims.x, dims.y, dimMin / 8f)
+
+            // Draw moving white box with red dot, moves with frame count
+            TF.SHAPES.color = Color.WHITE
+            TF.SHAPES.rect(rectPos.x - 16f, rectPos.y - 16f,
+                32f, 32f)
+            TF.SHAPES.color = Color.RED
+            TF.SHAPES.circle(rectPos, 3f)
+        }
+        // ============================= DRAW TEXT =============================
+        TF.BATCH.use {
+            //TODO use a smarter multi-line text rendering structure
+            TF.FONT.data.setScale(1.25f)
+            TF.FONT.color = Color.BLACK
+            TF.FONT.draw(TF.BATCH,
+                "FRAME: $frame",
+                rectPos.x, rectPos.y)
+
+            TF.FONT.data.setScale(2f)
+            TF.FONT.color = Color.RED
+            TF.FONT.draw(TF.BATCH,
+                "Viewport Screen: ${TF.VIEWPORT.screenWidth} x ${TF.VIEWPORT.screenHeight}",
+                32f, 132f)
+
+            TF.FONT.color = Color.BLUE
+            TF.FONT.draw(TF.BATCH,
+                "Viewport World: ${TF.VIEWPORT.worldWidth} x ${TF.VIEWPORT.worldHeight}",
+                32f, 164f)
+
+            TF.FONT.draw(TF.BATCH,
+                "dims (min): ${dims.x}, ${dims.y}",
+                32f, 196f)
+
+            TF.FONT.color = Color.WHITE
+            TF.FONT.draw(TF.BATCH,
+                "Graphics Density: ${Gdx.graphics.density}",
+                32f, 228f)
+
+            TF.FONT.draw(TF.BATCH,
+                "PPI X & Y: ${Gdx.graphics.ppiX}, ${Gdx.graphics.ppiY}",
+                32f, 260f)
+        }
+    }
+
+    /**
+     * `drawGame` private method
+     *---------------------------
+     * This draws the actual game.
+     */
+    private fun drawGame() {
+
     }
 
     /**
