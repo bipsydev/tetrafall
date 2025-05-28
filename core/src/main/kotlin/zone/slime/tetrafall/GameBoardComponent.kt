2001cea2d65b08fb2d7912c3b31a62473008ee08
Lily-Heather Crawford <lilithalicecrawford@gmail.com>
2025-05-28 13:41:06 -0500

    Create `GameBoardComponent` for Artemis-odb ECS
    Represents the data held on the Game Board (width and height, list of tetrominoes, and player-controlled tetromino)

---
+++ core/src/main/kotlin/zone/slime/tetrafall/GameBoardComponent.kt
@@ -0,0 +1,22 @@
+package zone.slime.tetrafall
+
+import com.artemis.Component
+import ktx.collections.GdxArray
+import ktx.collections.GdxIntArray
+
+/**
+ *******************************************************************************
+ * `GameBoardComponent` data class (Artemis Component)
+ *******************************************************************************
+ * This is a subclass of `com.artemis.Component`
+ */
+class GameBoardComponent(
+    /** Width of the board. */
+    val width: Int = 10,
+    /** Height of the board. */
+    val height: Int = 20,
+    /** Resizable array of Entity IDs for each Tetromino on the board. */
+    val tetrominoes: GdxIntArray = GdxIntArray(0),
+    /** The Entity ID of the current player-controlled Tetromino. -1 if none. */
+    var player: Int = -1
+) : Component()
