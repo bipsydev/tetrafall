c3ea83955885d2edeaa2e5372b690621af20203f
Lily-Heather Crawford <lilithalicecrawford@gmail.com>
2025-06-02 10:37:13 -0500

    Create GameBoardRenderSystem.kt
    basic template, takes a `SpriteBatach` and a `ShapeRenderer` to use for object rendering. Needs a custom `processSystem` for fine control of the rendering logic.

---
+++ core/src/main/kotlin/zone/slime/tetrafall/GameBoardRenderSystem.kt
@@ -0,0 +1,17 @@
+package zone.slime.tetrafall
+
+import com.artemis.BaseSystem
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+
+class GameBoardRenderSystem(
+    private val batch: SpriteBatch = TetraFall.BATCH,
+    private val shapes: ShapeRenderer = TetraFall.SHAPES
+) : BaseSystem() {
+
+
+    override fun processSystem() {
+        TODO("Not yet implemented")
+    }
+
+}
